acc0b1fc3fffa0a56a5ca5cdd14d48b63818c3cd
Jake Hennig <91110170+J-101@users.noreply.github.com>
2024-05-31 09:11:54 -0600

    MP1 Work

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -27,57 +27,89 @@ import qualified Prelude as P
 --- ### mytake
 
 -- don't forget to put the type declaration or you will lose points!
-mytake = undefined
+mytake :: Int -> [a] -> [a]
+mytake _ [] = [] --- If empty ret empty list, base case
+mytake n (x:xs) --- Get each number
+    | n <= 0 = []
+    | otherwise = x : mytake (n-1) xs
+--- Used the Basic Recursion slides, in particular mylength for reference
 
 --- ### mydrop
 
 -- don't forget to put the type declaration or you will lose points!
-mydrop = undefined
+mydrop :: Int -> [a] -> [a]
+mydrop _ [] = []
+mydrop n xs | n <= 0 = xs --- If n < 0, ret whole list
+mydrop n (_:xs) = mydrop (n-1) xs --- Drops first n elements
 
 --- ### rev
 
 -- don't forget to put the type declaration or you will lose points!
-rev = undefined
+rev :: [a] -> [a]
+rev xs = revAux [] xs
+  where revAux acc [] = acc --- Auxiliary function for reversing
+        revAux acc (x:xs) = revAux (x:acc) xs
+--- Accumulating recursion slide from notes used for reference
+--- StackOverflow reference: https://stackoverflow.com/questions/33283905/recursive-functions-that-reverse-a-list-in-haskell
 
 --- ### app
 
 -- don't forget to put the type declaration or you will lose points!
-app = undefined
+app :: [a] -> [a] -> [a]
+app [] ys = ys --- If first list emp, ret second
+app (x:xs) ys = x : app xs ys --- Append first list words to second list
 
 --- ### inclist
 
 -- don't forget to put the type declaration or you will lose points!
-inclist = undefined
+inclist :: Num a => [a] -> [a]
+inclist xs = [x + 1 | x <- xs] --- increments each element by 1
+--- Referenced for list comprehension info: https://wiki.haskell.org/List_comprehension
 
 --- ### sumlist
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist = undefined
+sumlist :: Num a => [a] -> a
+sumlist [] = 0 --- If empty sum is 0
+sumlist (x:xs) = x + sumlist xs --- Add head to current tail sum
+--- Used StackOverflow implementation: https://stackoverflow.com/questions/51279298/haskell-sum-up-the-first-n-elements-of-a-list
 
 --- ### myzip
 
 -- don't forget to put the type declaration or you will lose points!
-myzip = undefined
+myzip :: [a] -> [b] -> [(a, b)]
+myzip [] _ = []
+myzip _ [] = []
+myzip (x:xs) (y:ys) = (x, y) : myzip xs ys --- Pair head elements then recurse
 
 --- ### addpairs
 
 -- don't forget to put the type declaration or you will lose points!
-addpairs = undefined
+addpairs :: Num a => [a] -> [a] -> [a]
+addpairs xs ys = [x + y | (x, y) <- myzip xs ys] --- Sum up corresponding elements from lists
 
 --- ### ones
 
 -- don't forget to put the type declaration or you will lose points!
-ones = undefined
+ones :: [Integer]
+ones = 1 : ones --- Infinite list of ones
 
 --- ### nats
 
 -- don't forget to put the type declaration or you will lose points!
-nats = undefined
+nats :: [Integer]
+nats = enumFrom 0 --- Enumerates through numbers from 0
+--- Didn't need to use recursion for this func
 
 --- ### fib
 
 -- don't forget to put the type declaration or you will lose points!
-fib = undefined
+fib :: [Integer]
+fib = fibs
+  where
+    fibs = 0 : 1 : addpairs fibs (tail fibs) --- Fibonacci starts with 0 and 1 (base cases)
+--- Referenced Tail Recursion slides in notes
+--- Used this StackOverflow for reference: https://stackoverflow.com/questions/1105765/generating-fibonacci-numbers-in-haskell
 
 --- Set Theory
 --- ----------
@@ -85,22 +117,48 @@ fib = undefined
 --- ### add
 
 -- don't forget to put the type declaration or you will lose points!
-add = undefined
+add :: Ord a => a -> [a] -> [a]
+add x ys = insert  ys
+  where
+    insert  [] = [x] --- Where is an alternate syntax to defining beforehand
+    insert  (y:ys)
+      | x == y    = y : ys
+      | x > y     = y : insert ys
+      | otherwise = x : y : ys
 
 --- ### union
 
 -- don't forget to put the type declaration or you will lose points!
-union = undefined
+union :: Ord a => [a] -> [a] -> [a]
+union xs ys = insert xs ys
+  where
+    insert [] ys = ys
+    insert xs [] = xs
+    insert (x:xs) (y:ys)
+      | x < y     = x : insert xs (y:ys)
+      | x > y     = y : insert (x:xs) ys
+      | otherwise = x : insert xs ys
 
 --- ### intersect
 
 -- don't forget to put the type declaration or you will lose points!
-intersect = undefined
+intersect :: Ord a => [a] -> [a] -> [a]
+intersect xs ys = insert xs ys
+  where
+    insert [] _ = []
+    insert _ [] = []
+    insert (x:xs) (y:ys)
+      | x < y     = insert xs (y:ys)
+      | x > y     = insert (x:xs) ys
+      | otherwise = x : insert xs ys
 
 --- ### powerset
 
 -- don't forget to put the type declaration or you will lose points!
-powerset = undefined
+powerset :: Ord a => [a] -> [[a]]
+powerset [] = [[]]
+powerset (x:xs) = let ps = powerset xs
+                  in ps `union` [add x p | p <- ps]
 
 --- Higher Order Functions
 --- ----------------------
@@ -108,9 +166,29 @@ powerset = undefined
 --- ### inclist'
 
 -- don't forget to put the type declaration or you will lose points!
-inclist' = undefined
+myMap :: (a -> b) -> [a] -> [b]
+myMap _ [] = []
+myMap f (x:xs) = f x : myMap f xs  --- Apply func to head and recursively to tail
+--- StackOverflow custom map function reference: https://stackoverflow.com/questions/19216243/writing-a-custom-map-function
+--- Also used higher order functions slides for reference
+inclist' :: Num a => [a] -> [a]
+inclist' xs = myMap (+1) xs  --- Use myMap custom func to increment each element by 1
 
 --- ### sumlist'
 
 -- don't forget to put the type declaration or you will lose points!
-sumlist' = undefined
+myFold :: (b -> a -> b) -> b -> [a] -> b
+myFold _ acc [] = acc
+myFold f acc (x:xs) = myFold f (f acc x) xs
+
+sumlist' :: Num a => [a] -> a
+sumlist' xs = myFold (+) 0 xs  --- Use myFold to sum elements of the list
+
+{- References 
+Textbook: https://learnyouahaskell.com/chapters
+Recursion StackOverflow: https://stackoverflow.com/questions/14820138/understanding-recursion-in-haskell
+Class Slides/Lectures
+Function Explanations: https://www.haskell.org/tutorial/functions.html
+Used ChatGPT to assist with error checking
+More references provided along with some of the functions
+-}
--- mp1b-adts/src/Lib.hs
+++ mp1b-adts/src/Lib.hs
@@ -24,38 +24,76 @@ data Exp = IntExp Integer
 
 --- ### list2cons
 
+--- StackOverflow for reference on Cons: https://stackoverflow.com/questions/41558870/what-does-cons-and-mean-in-haskell
+
 -- don't forget to put the type declaration or you will lose points!
-list2cons = undefined
+list2cons :: [a] -> List a
+list2cons xs = insert (reverse xs) Nil
+  where
+    insert []     acc = acc
+    insert (x:xs) acc = insert xs (Cons x acc) --- Converts head and then does the tail
 
 --- ### cons2list
 
 -- don't forget to put the type declaration or you will lose points!
-cons2list = undefined
+cons2list :: List a -> [a]
+cons2list list = insert list []
+  where
+    insert Nil acc         = acc
+    insert (Cons x xs) acc = insert xs (acc `append` x)
+    append [] y        = [y] --- Prepends element to list then continues with the rest of list
+    append (y:ys) z    = y : append ys z --- Appends rest of list
 
 --- ### eval
 
 -- don't forget to put the type declaration or you will lose points!
-eval = undefined
+eval :: Exp -> Integer
+eval (IntExp n)   = n
+eval (PlusExp []) = 0
+eval (MultExp []) = 1
+eval (PlusExp es) = sum (map eval es) --- Evaluate + sum all sub-expressions from PlusExp
+eval (MultExp es) = product (map eval es) --- Evaluate + multiply all sub-expressions from MultExp
 
 --- ### list2cons'
 
 -- don't forget to put the type declaration or you will lose points!
-list2cons' = undefined
+list2cons' :: [a] -> List a
+list2cons' [] = Nil
+list2cons' (x:xs) = Cons x (list2cons xs) --- Creates list where first element is x, rest of list made recursively converting tail
 
 --- ### BinTree
 
--- BinTree
+data BinTree a = Node a (BinTree a) (BinTree a)
+               | Leaf
+  deriving (Show)
 
 --- ### sumTree
 
 -- don't forget to put the type declaration or you will lose points!
-sumTree = undefined
+sumTree :: Num a => BinTree a -> a
+sumTree Leaf = 0
+sumTree (Node x left right) = x + sumTree left + sumTree right --- Sums up all elements in a binary tree
 
 --- ### SimpVal
 
--- SimpVal
+data SimpVal = IntVal Integer
+             | BoolVal Bool
+             | StrVal String
+             | ExnVal String
+  deriving (Show)
 
 --- ### liftIntOp
 
 -- don't forget to put the type declaration or you will lose points!
-liftIntOp = undefined
+liftIntOp :: (Integer -> Integer -> Integer) -> SimpVal -> SimpVal -> SimpVal
+liftIntOp op (IntVal x) (IntVal y) = IntVal (x `op` y)
+liftIntOp _ _ _ = ExnVal "not an IntVal!"
+--- Operator StackOverflow Reference: https://stackoverflow.com/questions/40033307/operator-as-an-argument-in-haskell
+
+{- References 
+Textbook: https://learnyouahaskell.com/chapters
+Class Slides/Lectures (mainly week 2)
+Function Explanations: https://www.haskell.org/tutorial/functions.html
+Used ChatGPT to assist with error checking
+More references provided along with some of the functions
+-}
